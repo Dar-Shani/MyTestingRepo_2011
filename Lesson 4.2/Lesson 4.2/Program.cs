73ed8b4fa11656117d70c89fb4484228eb0987aa
Sarol <RedPaladin@yandex.ru>
2021-12-31 01:04:25 +0300

    Finishied
    Finishied

---
+++ Lesson 4.2/Lesson 4.2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lesson_4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите набор чисел разделённый пробелом");
+            string number = System.Console.ReadLine();
+            char[] count = new char[number.Length];
+            string sum = null;
+            int lvl = 0;
+            int sumTotal = 0;
+
+            for(int i = 0; i < number.Length; i++)
+            {
+                if (number[i] != ' ')
+                {
+                    
+                    count[lvl] = number[i];
+                    sum = new string(count);
+                    lvl++;
+
+                } else
+                {
+                    sumTotal += System.Convert.ToInt32(sum.ToString());
+                    sum = null;
+
+                    for (int b = 0; b < count.Length; b++)
+                        count[b] = default;
+                    lvl = 0;
+                }
+            }
+            sumTotal += System.Convert.ToInt32(sum.ToString());
+            System.Console.WriteLine($"Общая сумма {sumTotal}");
+        }
+    }
+}
---
+++ Lesson 4.3/Lesson 4.3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Lesson_4._3
+{
+    class Program
+    {
+        enum Season
+        {
+            Winter,
+            Spring,
+            Summer,
+            Autumn,
+        }
+
+        static String Seasonality (int _number)
+        {
+            if ((_number < 13 && _number > 10) || _number < 2)
+            {
+                return Convert.ToString(Season.Winter);
+            }
+            if (_number >= 2 && _number < 5)
+            {
+                return Convert.ToString(Season.Spring);
+            }
+            if (_number >= 5 && _number < 8)
+            {
+                return Convert.ToString(Season.Summer);
+            }
+            if (_number >= 8 && _number < 11)
+            {
+                return Convert.ToString(Season.Autumn);
+            } else
+            {
+                return "Ошибка: Введите число от 1 до 12";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Укажите месяц");
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+            
+            System.Console.WriteLine(Seasonality(number));  
+        }
+    }
+}
