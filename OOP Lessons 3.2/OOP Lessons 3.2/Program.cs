558160fec7ac7a93cb07ab21111a92dbcfb81cee
Sarol <RedPaladin@yandex.ru>
2022-04-01 20:40:45 +0300

    finished

---
+++ OOP Lessons 3.2/OOP Lessons 3.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OOP_Lessons_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Hello World!";
+            
+            Console.WriteLine($"текст до метода: {text}");
+            Console.WriteLine(Turnover(text));
+
+
+
+            Console.ReadLine();
+            
+            string Turnover(string _text)
+            {
+                int a, b;
+                char[] revers = new char[_text.Length];
+                for ( a = _text.Length, b = 0; b < _text.Length; a--, b++)
+                {
+                    revers[b] = Convert.ToChar(_text[a - 1]);
+                    
+                }
+                return new string(revers);
+            }
+        }
+    }
+}
---
+++ OOP Lessons 3.3/OOP Lessons 3.3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO; 
+
+namespace OOP_Lessons_3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string fileAdres = "C:\\Users\\Sarol\\source\\repos\\OOP Lessons 3.3\\OOP Lessons 3.3\\bin\\Debug\\netcoreapp3.1\\Контакты.txt";
+            string filetext = File.ReadAllText(fileAdres);
+
+            Console.WriteLine(filetext);
+
+            SearchMail(ref filetext);
+
+            Console.WriteLine("\n\nИтого \n" + filetext);
+            File.WriteAllText("Mail.txt", filetext);
+            Console.WriteLine("\nФайл сохранён");
+            Console.ReadLine();
+        }
+        public static void SearchMail(ref string s)
+        {
+            char[] divider = new char[s.Length];
+            int count = 0;
+            for (int a = 0; a < s.Length; a++)
+            {
+                if(s[a] == '%')
+                {
+                    for(int b = a + 2; b < s.Length; b++)
+                    {
+                        if ((s[b] != ' ') & (s[b] != '\n'))
+                        {
+                            divider[count] = s[b];
+                            count++;
+                        } else
+                        {
+                            divider[count] = '\n';
+                            count++;
+                            a = b + 1;
+                            break;
+                        } 
+                    }
+                }
+            }
+            s = new string(divider);
+        }
+    }
+}
